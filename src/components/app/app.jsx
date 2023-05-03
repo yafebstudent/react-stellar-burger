e10b498d78f756134fe4ff047d9f83fdfcb0e920
Stas <stsoosts@gmail.com>
2023-05-03 17:49:35 +0300

    refactor: delete unused elements

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,32 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import styles from './App.module.css';
-import data from '../../utils/data';
 import getIngredientsData from '../../utils/api/getIngredientsData';
 import AppHeader from '../app-header/AppHeader';
 import BurgerIngredients from '../burger-ingredients/BurgerIngredients';
-import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
+import BurgerConstructor from '../burger-constructor/BurgerConstructor';
 
 const App = () => {
   const [ingredientsData, setIngredientsData] = useState(null);
-  const [fetchError, setFetchError] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     let cleanupFunction = false;
 
-    setIsLoading(true);
     getIngredientsData()
       .then((responseData) => {
         if (!cleanupFunction) {
-          setIsLoading(false);
           setIngredientsData(responseData.data);
-          setFetchError('');
         }
       })
       .catch((error) => {
-        setIsLoading(false);
         setIngredientsData(null);
-        setFetchError(error.message);
+        // eslint-disable-next-line no-console
+        console.log(error.message);
       });
 
     // eslint-disable-next-line no-return-assign
