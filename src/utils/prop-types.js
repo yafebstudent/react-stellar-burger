728f9c8e775ea1089b6ca6d3ba0d2dc099e36f73
Stas <stsoosts@gmail.com>
2023-05-04 19:09:49 +0300

    fix: delete unuse ingredientPropType

--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -1,24 +1,21 @@
 import PropTypes from 'prop-types';
 import ingredientType from './types';
 
-export const ingredientDataPropType = {
-  ingredientData: ingredientType,
-};
 export const ingredientsDataPropType = {
   ingredientsData: PropTypes.arrayOf(ingredientType).isRequired,
 };
 export const BurgerIngredientPropType = {
   ingredientData: ingredientType,
-  setIngredientDetailsModalActive: PropTypes.func.isRequired,
+  openModal: PropTypes.func.isRequired,
   setactiveIngredientId: PropTypes.func.isRequired,
 };
 export const ModalPropType = {
-  modalActive: PropTypes.bool.isRequired,
-  setModalActive: PropTypes.func.isRequired,
+  isModalOpen: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
   children: PropTypes.element.isRequired,
 };
 export const ModalOverlayPropType = {
-  modalActive: PropTypes.bool.isRequired,
+  isModalOpen: PropTypes.bool.isRequired,
 };
 export const IngredientDetailsPropType = {
   activeIngredientData: ingredientType,
