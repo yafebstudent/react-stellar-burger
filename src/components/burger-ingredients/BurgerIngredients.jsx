c2da5d25fe8860900326d09272a11f28ac599ed6
Stas <stsoosts@gmail.com>
2023-05-06 23:06:26 +0300

    feat: implement ingredients tabs highlights

--- src/components/burger-ingredients/BurgerIngredients.jsx
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable no-restricted-globals */
+import React, { useState } from 'react';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import { useDispatch } from 'react-redux';
 import styles from './BurgerIngredients.module.css';
@@ -8,33 +9,45 @@ import IngredientDetails from '../ingredient-details/IngredientDetails';
 import useModal from '../../hooks/useModal';
 import { useGetIngredientsDataQuery } from '../../services/ingredientsDataAPI';
 import { clearActiveIngredientData } from '../../services/activeIngredientDataSlice';
+import getCurrentTabName from './getCurrentTabName';
 
 const BurgerIngredients = () => {
   const dispatch = useDispatch();
-  const [currentTab, setCurrentTab] = React.useState('buns');
+  const [currentTab, setCurrentTab] = useState('buns');
   const { isModalOpen, openModal, closeModal } = useModal();
   const { data: ingredientsResponseData } = useGetIngredientsDataQuery();
   const modalCloseButtonClickHandler = () => {
     closeModal();
     dispatch(clearActiveIngredientData());
   };
+  const scrollHandler = (event) => {
+    event.target.addEventListener('scroll', () => {
+      setCurrentTab(getCurrentTabName(styles.burgerIngredients__items));
+    });
+  };
+  const tabClickHandler = (tabName) => {
+    setCurrentTab(tabName);
+    document.querySelector(`#${tabName}`).scrollIntoView({ block: 'start', behavior: 'smooth' });
+  };
 
   return (
     <section className={`${styles.burgerIngredients} mb-10`}>
       <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
       <div className={`${styles.burgerIngredients__tabs} mb-10`}>
-        <Tab value="buns" active={currentTab === 'buns'} onClick={setCurrentTab}>
+        <Tab value="buns" active={currentTab === 'buns'} onClick={tabClickHandler}>
           Булки
         </Tab>
-        <Tab value="sauces" active={currentTab === 'sauces'} onClick={setCurrentTab}>
+        <Tab value="sauces" active={currentTab === 'sauces'} onClick={tabClickHandler}>
           Соусы
         </Tab>
-        <Tab value="toppings" active={currentTab === 'toppings'} onClick={setCurrentTab}>
+        <Tab value="toppings" active={currentTab === 'toppings'} onClick={tabClickHandler}>
           Начинки
         </Tab>
       </div>
-      <div className={styles.burgerIngredients__items}>
-        <h2 className="text text_type_main-medium mb-6">Булки</h2>
+      <div className={styles.burgerIngredients__items} onScroll={scrollHandler}>
+        <h2 className="text text_type_main-medium mb-6" id="buns">
+          Булки
+        </h2>
         <ul className={`${styles.burgerIngredientsList} pl-4`}>
           {ingredientsResponseData.data.map(
             (ingredientData) =>
@@ -47,7 +60,9 @@ const BurgerIngredients = () => {
               )
           )}
         </ul>
-        <h2 className="text text_type_main-medium mt-10 mb-6">Соусы</h2>
+        <h2 className="text text_type_main-medium mt-10 mb-6" id="sauces">
+          Соусы
+        </h2>
         <ul className={`${styles.burgerIngredientsList} pl-4`}>
           {ingredientsResponseData.data.map(
             (ingredientData) =>
@@ -60,7 +75,9 @@ const BurgerIngredients = () => {
               )
           )}
         </ul>
-        <h2 className="text text_type_main-medium mt-10 mb-6">Начинки</h2>
+        <h2 className="text text_type_main-medium mt-10 mb-6" id="toppings">
+          Начинки
+        </h2>
         <ul className={`${styles.burgerIngredientsList} pl-4`}>
           {ingredientsResponseData.data.map(
             (ingredientData) =>
