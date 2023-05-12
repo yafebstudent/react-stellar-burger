58e46da5a88e20e05ba2b88c9342b6e2b4b077ae
Stas <stsoosts@gmail.com>
2023-05-13 02:44:07 +0300

    fix: profile page

--- src/pages/profile-page/Profile.jsx
+++ src/pages/profile-page/Profile.jsx
@@ -1,18 +1,169 @@
 /* eslint-disable no-console */
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   Input,
+  Button,
   EmailInput,
   PasswordInput,
 } from '@ya.praktikum/react-developer-burger-ui-components';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import styles from './Profile.module.css';
 import setLinkActiveStyle from '../../utils/setLinkActiveStyle';
+import {
+  useGetUserDataQuery,
+  useLogOutMutation,
+  useUpdateUserDataMutation,
+} from '../../services/stellarBurgersAPI';
+import LoadingSpinner from '../../components/loading-spinner/LoadingSpinner';
 
 const Profile = () => {
+  let content;
+  const navigate = useNavigate();
+  const {
+    data: userDataResponseData,
+    isLoading,
+    isSuccess,
+    isFetching,
+    isError,
+    refetch,
+  } = useGetUserDataQuery(localStorage.getItem('accessToken') || '');
+  const [updateUserData] = useUpdateUserDataMutation();
+  const [logOut] = useLogOutMutation();
   const [nameValue, setNameValue] = useState('');
   const [emailValue, setEmailValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
+  const [isButtonsHidden, setIsButtonHidden] = useState('true');
+  const nameChangeHandler = (value) => {
+    setNameValue(value);
+    if (value && value !== userDataResponseData?.user.name) {
+      setIsButtonHidden(false);
+    } else {
+      setIsButtonHidden(true);
+    }
+  };
+  const emailChangeHandler = (value) => {
+    setEmailValue(value);
+    if (value && value !== userDataResponseData?.user.email) {
+      setIsButtonHidden(false);
+    } else {
+      setIsButtonHidden(true);
+    }
+  };
+  const passwordChangeHandler = (value) => {
+    setPasswordValue(value);
+    if (value && value.length >= 6) {
+      setIsButtonHidden(false);
+    } else {
+      setIsButtonHidden(true);
+    }
+  };
+  const cancelHandler = () => {
+    setNameValue(userDataResponseData?.user.name);
+    setEmailValue(String(userDataResponseData?.user.email));
+    setIsButtonHidden(true);
+  };
+  const saveInputsValues = () => {
+    const newUserProfileData = {
+      ...(nameValue !== userDataResponseData?.user.name && { name: nameValue }),
+      ...(emailValue !== userDataResponseData?.user.email && { email: emailValue }),
+      ...(passwordValue && { password: passwordValue }),
+    };
+
+    updateUserData(newUserProfileData)
+      .then((data) => {
+        console.log(data);
+        setIsButtonHidden(true);
+        setPasswordValue('');
+        refetch();
+      })
+      .catch((error) => console.error(error));
+  };
+  const logOutHandler = useCallback(
+    (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      logOut()
+        .then((data) => {
+          if (data in data) {
+            console.log(data.data.message);
+          }
+          localStorage.removeItem('refreshToken');
+          localStorage.removeItem('accessToken');
+          navigate('/login', { replace: true });
+        })
+        .catch((error) => console.error(error));
+    },
+    [logOut, navigate]
+  );
+
+  useEffect(() => {
+    if (isSuccess) {
+      setNameValue(userDataResponseData?.user.name);
+      setEmailValue(String(userDataResponseData?.user.email));
+    }
+  }, [isSuccess, userDataResponseData]);
+
+  if (isError)
+    content = <p className="text text_type_main-medium">An error has occurred with user data!</p>;
+  if (isLoading || isFetching) content = <LoadingSpinner />;
+  if (isSuccess) {
+    content = (
+      <>
+        <li>
+          <Input
+            type="text"
+            placeholder="Имя"
+            onChange={(event) => nameChangeHandler(event.target.value)}
+            icon="EditIcon"
+            value={nameValue}
+            name="name"
+            error={false}
+            errorText="Ошибка"
+            size="default"
+            extraClass="mb-6"
+          />
+        </li>
+        <li>
+          <EmailInput
+            onChange={(event) => emailChangeHandler(event.target.value)}
+            value={emailValue}
+            name="email"
+            placeholder="Логин"
+            icon="EditIcon"
+            extraClass="mb-6"
+          />
+        </li>
+        <li>
+          <PasswordInput
+            placeholder="Пароль"
+            onChange={(event) => passwordChangeHandler(event.target.value)}
+            value={passwordValue}
+            name="password"
+            extraClass="mb-6"
+            icon="EditIcon"
+          />
+        </li>
+        <li className={styles.buttons}>
+          {!isButtonsHidden && (
+            <>
+              <Button onClick={cancelHandler} htmlType="button" type="secondary" size="medium">
+                Отмена
+              </Button>
+              <Button
+                onClick={saveInputsValues}
+                htmlType="submit"
+                type="primary"
+                size="medium"
+                extraClass="mb-20"
+              >
+                Сохранить
+              </Button>
+            </>
+          )}
+        </li>
+      </>
+    );
+  }
 
   return (
     <main className={styles.main}>
@@ -37,7 +188,12 @@ const Profile = () => {
             </NavLink>
           </li>
           <li className={styles.navigationListItem}>
-            <NavLink className="text text_type_main-medium" style={setLinkActiveStyle} to="/">
+            <NavLink
+              onClick={(event) => logOutHandler(event)}
+              className="text text_type_main-medium"
+              style={setLinkActiveStyle}
+              to="/login"
+            >
               Выход
             </NavLink>
           </li>
@@ -46,37 +202,7 @@ const Profile = () => {
           В этом разделе вы можете изменить свои персональные данные
         </p>
       </nav>
-      <ul>
-        <li>
-          <Input
-            type="text"
-            placeholder="Имя"
-            onChange={(event) => setNameValue(event.target.value)}
-            icon="EditIcon"
-            value={nameValue}
-            name="name"
-            error={false}
-            errorText="Ошибка"
-            size="default"
-            extraClass="mb-6"
-          />
-          <EmailInput
-            onChange={(event) => setEmailValue(event.target.value)}
-            value={emailValue}
-            name="email"
-            placeholder="E-mail"
-            isIcon={false}
-            extraClass="mb-6"
-            icon="EditIcon"
-          />
-          <PasswordInput
-            onChange={(event) => setPasswordValue(event.target.value)}
-            value={passwordValue}
-            name="password"
-            icon="EditIcon"
-          />
-        </li>
-      </ul>
+      <ul className={styles.userDataList}>{content}</ul>
     </main>
   );
 };
