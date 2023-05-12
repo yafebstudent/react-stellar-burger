0248f8d73fff6e9f46e1edada56d204be1825f64
Stas <stsoosts@gmail.com>
2023-05-13 02:32:07 +0300

    fix: app routes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -8,6 +8,9 @@ import ResetPasswordPage from '../../pages/reset-password-page/ResetPasswordPage
 import ForgotPasswordPage from '../../pages/forgot-password-page/ForgotPasswordPage';
 import Page404 from '../../pages/page-404/Page404';
 import Profile from '../../pages/profile-page/Profile';
+import ProtectedRouteElement from '../protected-route-element/ProtectedRouteElement';
+import IngredientPage from '../../pages/ingredient-page/IngredientPage';
+import IngredientDetails from '../ingredient-details/IngredientDetails';
 
 const App = () => {
   return (
@@ -19,7 +22,12 @@ const App = () => {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/reset-password" element={<ResetPasswordPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<ProtectedRouteElement element={<Profile />} />}>
+            <Route path="/profile/orders" element={<main>{null}</main>} />
+          </Route>
+          <Route path="/ingredients" element={<IngredientPage />}>
+            <Route path=":id" element={<IngredientDetails />} />
+          </Route>
           <Route path="*" element={<Page404 />} />
         </Route>
       </Routes>
