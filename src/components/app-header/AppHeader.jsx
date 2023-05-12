8590bad7d2b57408e1a452fd4288214417eec63b
Stas <stsoosts@gmail.com>
2023-05-12 11:44:39 +0300

    Revert "feat: implement page 404"
    This reverts commit 84eb3c91c137d4b381a8390ebb8a0dfe14339d4b.

--- src/components/app-header/AppHeader.jsx
+++ src/components/app-header/AppHeader.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import {
   BurgerIcon,
   ListIcon,
@@ -14,27 +13,27 @@ const AppHeader = () => {
       <nav className={styles.navbar}>
         <ul className={`${styles.navbarList} pt-4 pb-4`}>
           <li>
-            <NavLink to="/" className={`${styles.navbarList__link} pt-4 pr-5 pb-4 pl-5`}>
+            <a href="/" className={`${styles.navbarList__link} pt-4 pr-5 pb-4 pl-5`}>
               <BurgerIcon type="secondary" />
               <span className="text text_type_main-default ml-2">Конструктор</span>
-            </NavLink>
+            </a>
           </li>
           <li>
-            <NavLink to="/" className={`${styles.navbarList__link} pt-4 pr-5 pb-4 pl-5`}>
+            <a href="/" className={`${styles.navbarList__link} pt-4 pr-5 pb-4 pl-5`}>
               <ListIcon type="secondary" />
               <span className="text text_type_main-default ml-2">Лента заказов</span>
-            </NavLink>
+            </a>
           </li>
           <li className={styles.logo}>
-            <NavLink to="/" className={styles.logo__link}>
+            <a href="/" className={styles.logo__link}>
               <Logo />
-            </NavLink>
+            </a>
           </li>
           <li className={styles.profile}>
-            <NavLink to="/profile" className={`${styles.navbarList__link} pt-4 pr-5 pb-4 pl-5`}>
+            <a href="/" className={`${styles.navbarList__link} pt-4 pr-5 pb-4 pl-5`}>
               <ProfileIcon type="secondary" />
               <span className="text text_type_main-default ml-2">Личный кабинет</span>
-            </NavLink>
+            </a>
           </li>
         </ul>
       </nav>
