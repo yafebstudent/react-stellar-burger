1c74bd37d6d0abe91c8784ce32aaf7f07094803b
Stas <stsoosts@gmail.com>
2023-05-13 02:40:55 +0300

    feat: auth responses

--- src/services/stellarBurgersAPI.js
+++ src/services/stellarBurgersAPI.js
@@ -1,11 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import baseUrl from '../utils/api/constants';
+import baseUrl from '../utils/apiConstants';
 
 export const stellarBurgersAPI = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (build) => ({
     getIngredientsData: build.query({
       query: () => 'ingredients',
+    }),
+    getUserData: build.query({
+      query: (accessToken) => ({
+        url: 'auth/user',
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }),
     }),
     getOrderData: build.mutation({
       query: (payload) => ({
@@ -47,13 +56,50 @@ export const stellarBurgersAPI = createApi({
         },
       }),
     }),
+    authUser: build.mutation({
+      query: (payload) => ({
+        url: 'auth/login',
+        method: 'POST',
+        body: payload,
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }),
+    }),
+    logOut: build.mutation({
+      query: () => ({
+        url: 'auth/logout',
+        method: 'POST',
+        body: {
+          token: localStorage.getItem('refreshToken') || '',
+        },
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }),
+    }),
+    updateUserData: build.mutation({
+      query: (payload) => ({
+        url: 'auth/user',
+        method: 'PATCH',
+        body: payload,
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          Authorization: `Bearer ${localStorage.getItem('accessToken') || ''}`,
+        },
+      }),
+    }),
   }),
 });
 
 export const {
   useGetIngredientsDataQuery,
+  useGetUserDataQuery,
   useGetOrderDataMutation,
   useGetResetEmailMutation,
   useResetPasswordMutation,
   useRegisterUserMutation,
+  useAuthUserMutation,
+  useLogOutMutation,
+  useUpdateUserDataMutation,
 } = stellarBurgersAPI;
