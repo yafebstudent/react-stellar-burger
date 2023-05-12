b88afa6de7e6da45876955169a32da17e666137a
Stas <stsoosts@gmail.com>
2023-05-12 11:44:55 +0300

    Revert "feat: implement MainPage"
    This reverts commit 221e1f4b557363e0368741ed5207f71ab5fa11d5.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from '../../pages/MainPage';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
+import styles from './App.module.css';
+import AppHeader from '../app-header/AppHeader';
+import BurgerIngredients from '../burger-ingredients/BurgerIngredients';
+import BurgerConstructor from '../burger-constructor/BurgerConstructor';
+import { useGetIngredientsDataQuery } from '../../services/ingredientsDataAPI';
+import LoadingSpinner from '../loading-spinner/LoadingSpinner';
 
 const App = () => {
+  const { isError, isLoading, isFetching, isSuccess } = useGetIngredientsDataQuery();
+  let content;
+
+  if (isError) content = <h4>An error has occurred with ingredients data!</h4>;
+  if (isLoading || isFetching) content = <LoadingSpinner />;
+  if (isSuccess) {
+    content = (
+      <DndProvider backend={HTML5Backend}>
+        <BurgerIngredients />
+        <BurgerConstructor />
+      </DndProvider>
+    );
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div className={styles.app}>
+      <AppHeader />
+      <main>
+        <div className="outerWrapper">{content}</div>
+      </main>
+    </div>
   );
 };
 
